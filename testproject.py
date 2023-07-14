bc96c4431ea8e8c8dfd916655b0a498aa5129031
root <root@DESKTOP-CU0CR7L.localdomain>
2023-07-14 14:25:23 +0100

    new change

--- testproject.py
+++ testproject.py
@@ -1,4 +1,8 @@
-for row in range(6):
+num = int(input(" enster the number "))
+if num == 1:
+
+
+  for row in range(6):
     for col in range(5):
         if (col == 0 or col == 4) or (row == 0 or row == 3 and  (col >0 and col < 4)):
             print("*",end=" ")
